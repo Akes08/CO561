b0c861cc7b09ddbf521134497d9e5b5b22baf938
Akes.Ali <ak5210365@gmail.com>
2023-05-09 16:04:47 +0100

    Initial commit

---
+++ bank/account.fs
@@ -0,0 +1,36 @@
+﻿module account
+
+type Account(accountNumber:string, initialBalance:float) = //constructor
+    let mutable balance = initialBalance //changeable value
+
+    member this.AccountNumber = accountNumber
+    member this.Balance = balance
+    
+    //This shows what accounts that will be displayed
+    static member accounts =
+        [ Account("22012359", 50.0)
+          Account("22113399", 80.0)
+          Account("22014785", 40.0)
+          Account("22156844", 0.0)
+          Account("22168974", 250.0)
+          Account("22369755", 8.0)]
+
+//This withdraws funds
+    member this.Withdrawal(amount:float) = // Data type & Account balance float when it changes
+        if amount <= balance then // Passing data
+            printfn "Transaction complete" // Print successful transaction
+        else
+           printfn "Transaction failed, Insufficient funds" // Print out unsuccessful transaction
+   
+   //This deposits funds
+    member this.Deposit(amount:float) = // data type
+        balance <- balance + amount // pass data to balance
+        
+    member this.Print() =
+        printfn "Account number: %s, Balance: %f" accountNumber balance // System will print out balance after deposit
+
+let CheckAccount(balance:float) = // Function to check balance
+    match balance with
+    | b when b < 10.0 -> printfn "Balance is low"
+    | b when b >= 10.0 && b <= 100.0 -> printfn "Balance is OK"
+    | _ -> printfn "Balance is high" // System outputs when output is matched
---
+++ bookseat/bookseat.fs
@@ -0,0 +1,22 @@
+﻿module bookseat
+
+type Ticket = {seat:int; customer:string} // Constructor
+
+let mutable tickets = [for n in 1..10 -> {seat = n; customer = ""}] // changeable value
+
+//This displayticket function allows the seat number and customer be displayed together
+let DisplayTicket () =
+    printfn "%-5s %-15s" "Number of Seats" "Customer Name" // Print seats and customers name
+    printfn "%-5s %-15s" "_______________---_---_______--" // The display on how the information will look
+    for ticket in tickets do
+        printfn "  %-5d   %-15s" ticket.seat ticket.customer
+
+let BookSeat (customers:string, seats:int) = 
+    let assignCustomer (ticket:Ticket) =
+        if ticket.seat = seats then
+            { ticket with customer = customers}
+        else
+            ticket
+    tickets <- List.map assignCustomer tickets //books seat for customer
+
+
